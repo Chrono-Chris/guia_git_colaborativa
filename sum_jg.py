ef8effd2c2c179e3555d13c314edc9ccee05f569
Chrono-Chris <chmunozsaldias@gmail.com>
2026-09-08 21:18:25 -0300

    sum_jg.py

--- sum_jg.py
+++ sum_jg.py
@@ -1,6 +1,6 @@
 def suma(a: int | float, b: int | float) -> int | float:
     """
-    Sumo a+b por que soy un crack y lo hago función.
+    Sumo a+b por que soy un crack y lo hago función. ÑOÑO
     """
     if isinstance(a, (int, float)) and isinstance(b, (int, float)):
         return a + b
